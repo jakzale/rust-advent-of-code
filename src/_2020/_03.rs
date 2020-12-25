9a4c51d5b97f4804cc4b06ce2ba3095f720d3883
Jakub Zalewski <jakzale@gmail.com>
2020-12-25 21:39:56 +0100

    Add implementation for 2020 day 03

--- src/_2020/_03.rs
+++ src/_2020/_03.rs
@@ -44,6 +44,12 @@ pub struct Grid {
 
 // API
 impl Grid {
+    pub fn from_str(grid: &str) -> Self {
+        let rows: Vec<Row> = grid.split_whitespace().map(|x| Row::from_str(x)).collect();
+        Grid {
+            rows,
+        }
+    }
     pub fn height(self: &Self) -> usize {
         return self.rows.len();
     }
@@ -54,6 +60,14 @@ impl Grid {
         }
 
         return self.rows[row].is_tree(column);
+    }
+
+    pub fn count_trees(self: &Self) -> usize {
+        (1..self.height())
+            .map(|x| (x, x * 3))
+            .map(|(r,c)| self.is_tree(r,c))
+            .map(|t| if t { 1 } else { 0 })
+            .sum()
     }
 }
 
@@ -78,5 +92,47 @@ mod tests {
         assert_eq!(false, row.is_tree(9));
         assert_eq!(true, row.is_tree(1));
         assert_eq!(true, row.is_tree(10));
+    }
+
+    #[test]
+    fn empty_grid() {
+        let grid = Grid::from_str("");
+
+        assert_eq!(0, grid.height());
+        assert_eq!(false, grid.is_tree(0, 0));
+        assert_eq!(false, grid.is_tree(1, 3));
+        assert_eq!(false, grid.is_tree(2, 6));
+        assert_eq!(false, grid.is_tree(3, 9));
+    }
+
+    #[test]
+    fn sample_grid() {
+        let input =
+            r#"..##.......
+               #...#...#..
+               .#....#..#.
+               ..#.#...#.#
+               .#...##..#.
+               ..#.##.....
+               .#.#.#....#
+               .#........#
+               #.##...#...
+               #...##....#
+               .#..#...#.#"#;
+        let grid = Grid::from_str(input);
+
+        assert_eq!(11, grid.height());
+        assert_eq!(false, grid.is_tree(1,  3));
+        assert_eq!(true,  grid.is_tree(2,  6));
+        assert_eq!(false, grid.is_tree(3,  9));
+        assert_eq!(true,  grid.is_tree(4,  12));
+        assert_eq!(true,  grid.is_tree(5,  15));
+        assert_eq!(false, grid.is_tree(6,  18));
+        assert_eq!(true,  grid.is_tree(7,  21));
+        assert_eq!(true,  grid.is_tree(8,  24));
+        assert_eq!(true,  grid.is_tree(9,  27));
+        assert_eq!(true,  grid.is_tree(10, 30));
+
+        assert_eq!(7, grid.count_trees());
     }
 }
